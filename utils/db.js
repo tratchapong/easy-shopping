72a443dc21fddcf23ca3676b90ab722656e615d3
tratchapong <tratchapong@gmail.com>
2021-03-20 16:34:02 +0700

    change db name to easy-shopping

--- utils/db.js
+++ utils/db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host : 'localhost',
   user : 'root',
   password : '123456789',
-  database : 'easy_shopping'
+  database : 'easy-shopping'
 })
 
 module.exports = pool.promise()
